776d2fa3ddd47e0dc761940c7319138e7e9072cb
GCRA101 <giorgio.albieri@burohappold.com>
2025-01-27 12:31:39 +0000

    Change ViewType to RevitViewType to avoid overlaps with Revit api class

---
+++ model/revit/RevitViewType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReuseSchemeTool.model.revit
+{
+    public enum RevitViewType
+    {
+        THREE_D,
+        PLAN,
+        DRAFTING,
+        SECTION,
+        SHEET,
+        TABLE
+    }
+}
