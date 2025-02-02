ea3f54aee77462091bd6b3945b58c524c956ee3b
GCRA101 <giorgio.albieri@burohappold.com>
2025-02-02 20:28:29 +0000

    Add features to the ViewSheetBuilder class

--- model/revit/ViewSheetBuilder.cs
+++ model/revit/ViewSheetBuilder.cs
@@ -12,20 +12,24 @@ namespace ReuseSchemeTool.model.revit
     {
         /* ATTRIBUTES */
         private static Autodesk.Revit.DB.ViewSheet viewSheet=null;
+        private static Autodesk.Revit.DB.Element titleBlock = null;
 
         /* METHODS */
-        public static void initialise(ViewSheet placeholder)
+        public static void initialise(ViewSheet placeholder, string number, string title)
         {
             viewSheet = placeholder;
+            viewSheet.SheetNumber = number;
+            viewSheet.Name = title;
         }
 
         public static void buildTitleBlock(string titleBlockTypeName)
         {
             if (viewSheet == null) { return; }
 
-            if (searchTitleBlockIdByName(titleBlockTypeName)!=null) 
+            if (searchTitleBlockByName(titleBlockTypeName)!=null) 
             {
-                viewSheet.ConvertToRealSheet(searchTitleBlockIdByName(titleBlockTypeName));
+                Autodesk.Revit.DB.Element titleBlock = searchTitleBlockByName(titleBlockTypeName);
+                viewSheet.ConvertToRealSheet(titleBlock.Id);
             }
 
         }
@@ -41,27 +45,74 @@ namespace ReuseSchemeTool.model.revit
         {
             if (viewSheet == null) { return; }
 
-            int deltaUNum=Enum.GetValues(typeof(SheetColumn)).Length;
+
+            int deltaUNum = Enum.GetValues(typeof(SheetColumn)).Length;
             int deltaVNum = Enum.GetValues(typeof(SheetRow)).Length;
 
-            double x = viewSheet.Outline.Max.U / deltaUNum * ((int)location.column+1);
-            double y = viewSheet.Outline.Max.V / deltaVNum * ((int)location.row+1);
+
+
+            // VIEWSHEET OUTLINE DIMENSIONS
+
+            // Calculate the width and height in feet
+            double outlineWidth_ft = viewSheet.Outline.Max.U - viewSheet.Outline.Min.U;
+            double outlineHeight_ft = viewSheet.Outline.Max.V - viewSheet.Outline.Min.V;
+
+            // Convert from feet to millimeters
+            double outlineWidth_mm = UnitUtils.ConvertFromInternalUnits(outlineWidth_ft, UnitTypeId.Millimeters);
+            double outlineHeight_mm = UnitUtils.ConvertFromInternalUnits(outlineHeight_ft, UnitTypeId.Millimeters);
+
+
+            // Adjust the crop box of the view
+            BoundingBoxXYZ cropBox = view.CropBox;
+
+            double cropBoxWidth_ft = cropBox.Max.X - cropBox.Min.X;
+            double cropBoxHeight_ft = cropBox.Max.Y - cropBox.Min.Y;
+
+            // Convert from feet to millimeters
+            double cropBoxWidth_mm = UnitUtils.ConvertFromInternalUnits(cropBoxWidth_ft, UnitTypeId.Millimeters);
+            double cropBoxHeight_mm = UnitUtils.ConvertFromInternalUnits(cropBoxHeight_ft, UnitTypeId.Millimeters);
+
+            double deltaX = (((int)size.width) * ((outlineWidth_ft / deltaUNum)/cropBoxWidth_ft) * cropBoxWidth_ft - cropBoxWidth_ft)/ 2;
+            double deltaY = (((int)size.height) * ((outlineHeight_ft / deltaVNum)/cropBoxHeight_ft) * cropBoxHeight_ft - cropBoxHeight_ft )/ 2;
+
+            cropBox.Max = new XYZ(cropBox.Max.X + deltaX, cropBox.Max.Y + deltaY, cropBox.Max.Z); // Increase size
+            cropBox.Min = new XYZ(cropBox.Min.X - deltaX, cropBox.Min.Y - deltaY, cropBox.Min.Z); // Decrease size
+            
+            view.CropBox = cropBox;
+
+
+
+
+            //double x = viewSheet.Outline.Max.U-(viewSheet.Outline.Max.U / deltaUNum * ((int)location.column+1));
+            double x = viewSheet.Outline.Max.U / deltaUNum * ((int)location.column);
+            double y = viewSheet.Outline.Max.V-(viewSheet.Outline.Max.V / deltaVNum * ((int)location.row));
 
             XYZ point = new XYZ(x, y, 0);
 
-            Viewport.Create(viewSheet.Document, viewSheet.Id, view.Id, point);
+            Viewport viewport = Viewport.Create(viewSheet.Document, viewSheet.Id, view.Id, point);
+
+
+            //// TITLEBLOCK DIMENSIONS
+
+            //// Get the width and height parameters
+            //double ttlbkWidth = titleBlock.get_Parameter(BuiltInParameter.SHEET_WIDTH).AsDouble();
+            //double ttlbkHeight = titleBlock.get_Parameter(BuiltInParameter.SHEET_HEIGHT).AsDouble();
+            
+            //// Convert from feet to millimeters (if needed)
+            //double ttlbkWidth_mm = UnitUtils.ConvertFromInternalUnits(ttlbkWidth, UnitTypeId.Millimeters);
+            //double ttlbkHeight_mm = UnitUtils.ConvertFromInternalUnits(ttlbkHeight, UnitTypeId.Millimeters);
+
+
         }
 
         
-        private static ElementId searchTitleBlockIdByName(string titleBlockTypeName)
+        private static Element searchTitleBlockByName(string titleBlockTypeName)
         {
             return new FilteredElementCollector(viewSheet.Document)
                     .WhereElementIsElementType()
                     .OfCategory(BuiltInCategory.OST_TitleBlocks)
                     .ToList()
-                    .First(tb => tb.Name == titleBlockTypeName)
-                    .Id;
-
+                    .First(tb => tb.Name == titleBlockTypeName);
         }
 
         public static ViewSheet getViewSheet()
