c0884f93e4c043fb773d6c0a76b67561f29578fa
GCRA101 <giorgio.albieri@burohappold.com>
2025-02-18 10:08:14 +0000

    Add FileManagers in the model and in the controller packages.

---
+++ model/FileManager.cs
@@ -0,0 +1,83 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace ReuseSchemeTool.model
+{
+
+    public static class FileManager
+    {
+
+    //METHODS
+    public static string setDatedFolderPath(string folderPath, string subFolderName)
+        {
+            DateTime dateObj = DateTime.Now;
+            string datedFolderPath = "";
+
+            //Add current time and date in front of subfolder name
+            datedFolderPath = folderPath + "\\" + dateObj.Year.ToString() + dateObj.Month.ToString("D2") +
+                dateObj.Day.ToString("D2") + dateObj.Hour.ToString() + dateObj.Minute.ToString() + "_" + subFolderName;
+
+            return datedFolderPath;
+        }
+
+
+        public static string setNewFilePath(string oldFilePath, string acronym)
+        {
+            string newFilePath;
+            DateTime dateObj = DateTime.Now;
+
+            char[] sep = { '/', '\\', '/' };
+
+            /* Add Label in front of file name containing info about current date and iteration number.
+               File gets saved in the same folder of the original */
+            string[] parts = oldFilePath.Split(sep, StringSplitOptions.None);
+            string lastPart = parts.Last();
+            newFilePath = oldFilePath.Remove(oldFilePath.LastIndexOf(lastPart)) + acronym +
+                          dateObj.Year.ToString() + dateObj.Month.ToString("D2") + dateObj.Day.ToString("D2") + "_" +
+                          dateObj.Hour.ToString("D2") + dateObj.Minute.ToString("D2") + "_" + lastPart;
+
+            return newFilePath;
+        }
+
+        public static string setNewFilePath(string oldFilePath, string acronym, int iterNum)
+        {
+            string newFilePath;
+            DateTime dateObj = DateTime.Now;
+
+            char[] sep = { '/', '\\', '/' };
+
+            /* Add Label in front of file name containing info about current date and iteration number.
+               File gets saved in the same folder of the original */
+            string[] parts = oldFilePath.Split(sep, StringSplitOptions.None);
+            string lastPart = parts.Last();
+            newFilePath = oldFilePath.Remove(oldFilePath.LastIndexOf(lastPart)) + acronym +
+                          dateObj.Year.ToString() + dateObj.Month.ToString("D2") + dateObj.Day.ToString("D2") + "_" +
+                          dateObj.Hour.ToString("D2") + dateObj.Minute.ToString("D2") + "_" + "Iteration_" +
+                          iterNum.ToString() + "_" + lastPart;
+
+            return newFilePath;
+        }
+
+
+        public static string setNewFilePath(string oldFilePath, string destinationFolderPath, string acronym, int iterNum)
+        {
+            string newFilePath;
+            DateTime dateObj = DateTime.Now;
+
+            //Add Label in front of file name containing info about current date and iteration number.
+            //File gets saved in a different folder than the original
+
+            newFilePath = destinationFolderPath + "\\" + acronym + dateObj.Year.ToString() + dateObj.Month.ToString("D2") +
+                dateObj.Day.ToString("D2") + "_" + "Iteration_" + iterNum.ToString();
+
+            return newFilePath;
+
+        }
+
+    }
+
+}
