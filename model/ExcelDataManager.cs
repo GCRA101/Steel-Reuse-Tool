49cf469197485a41da685aa469464f4f3e1fb92a
GCRA101 <giorgio.albieri@burohappold.com>
2025-02-20 16:09:48 +0000

    Adapt ExcelDataManager methods to work on Database_Graphs embedded excel file.

--- model/ExcelDataManager.cs
+++ model/ExcelDataManager.cs
@@ -15,6 +15,8 @@ public class ExcelDataManager
     private Worksheet ExcelWks;
 
     private string filePath;
+    private string outputsFolderPath;
+
 
     // CONSTRUCTORS
     public ExcelDataManager(string filePath = "")
@@ -24,30 +26,33 @@ public class ExcelDataManager
         {
             throw new InvalidFilePathException("The filepath provided doesn't correspond to an excel file.");
         }
+
         // Assign the filePath attribute with the filePath string input if this corresponds to an excel file
         this.filePath = filePath;
     }
 
-    public void openEmbeddedExcelFile(string embeddedFilePath, string outputFilePath)
+    public ExcelDataManager(string embeddedfilePath, string outputsFolderPath) 
     {
-        Stream excelStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(filePath);
+        // Throw an exception if the input filepath provided doesn't correspond to an excel file
+        if (embeddedfilePath != "" && !(embeddedfilePath.Contains(".xlsx") || embeddedfilePath.Contains(".xlsm") || embeddedfilePath.Contains(".xls")))
+        {
+            throw new InvalidFilePathException("The filepath provided doesn't correspond to an excel file.");
+        }
 
-        // Create a temporary file
-        File.WriteAllBytes(outputFilePath, ReadFully(excelStream));
+        Stream excelStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(embeddedfilePath);
 
-        // Open the Excel file using Interop
-        Application excelApp = new Application();
-        excelApp.Visible = true;
-        Workbook workbook = excelApp.Workbooks.Open(outputFilePath);
-
-        workbook.Close(true);
-        excelApp.Quit();
-        
+        if (excelStream != null)
+        {
+            // Create a temporary file
+            string tempFilePath = outputsFolderPath + "\\" + Path.GetFileName(embeddedfilePath);
+            File.WriteAllBytes(tempFilePath, readFully(excelStream));
+            this.filePath = tempFilePath;
+            return;
+        }
     }
 
-
     // Helper method to read the stream fully
-    private static byte[] ReadFully(Stream input)
+    private static byte[] readFully(Stream input)
     {
         using (MemoryStream ms = new MemoryStream())
         {
@@ -59,11 +64,11 @@ public class ExcelDataManager
 
 
     // METHODS
-    public void Initialize()
+    public void initialize(bool excelAppVisible = false)
     {
         // 1. INITIALIZE EXCEL APPLICATION
         ExcelApp = new Application();
-        ExcelApp.Visible = true;
+        ExcelApp.Visible = excelAppVisible;
 
         // 2. OPEN/CREATE EXCEL WORKBOOK
         if (filePath == "")
@@ -76,78 +81,73 @@ public class ExcelDataManager
         }
     }
 
-    //public void Write(string worksheetName, List<FrameDecorator> data, string headerTitle = "")
-    //{
-    //    // 1. OPEN/CREATE EXCEL WORKSHEET
-    //    if (ExcelWks == null)
-    //    {
-    //        if (ExcelWkb.Worksheets.Cast<Worksheet>().Select(wks => wks.Name).Contains(worksheetName))
-    //        {
-    //            ExcelWks = ExcelWkb.Worksheets[worksheetName];
-    //        }
-    //        else
-    //        {
-    //            ExcelWks = ExcelWkb.Worksheets.Add();
-    //            ExcelWks.Name = worksheetName;
-    //            ExcelWkb.Sheets["Sheet1"].Delete();
-    //        }
-    //        //ExcelWks.Tab.ColorIndex = 6;
-    //        ExcelWks.Activate();
-    //    }
 
-    //    // 2. POSITION ACTIVE CELL
-    //    if (ExcelWks.Range["B4"].Value == null)
-    //    {
-    //        ExcelWks.Range["B4"].Activate();
-    //    }
-    //    else
-    //    {
-    //        ExcelWks.Range["B4"].End[XlDirection.xlToRight].Offset[0, 1].Activate();
-    //    }
+    private void activateWorksheet(string worksheetName)
+    {
+        // 1. OPEN/CREATE EXCEL WORKSHEET
+        if (ExcelWks == null)
+        {
+            if (ExcelWkb.Worksheets.Cast<Worksheet>().Select(wks => wks.Name).Contains(worksheetName))
+            {
+                ExcelWks = ExcelWkb.Worksheets[worksheetName];
+            }
+            else
+            {
+                ExcelWks = ExcelWkb.Worksheets.Add();
+                ExcelWks.Name = worksheetName;
+                ExcelWkb.Sheets["Sheet1"].Delete();
+            }
+            ExcelWks.Tab.ColorIndex = (XlColorIndex)6;
+            ExcelWks.Activate();
+        }
+    }
 
-    //    // 3. WRITE DATA IN THE WORKSHEET
-    //    for (int i = 0; i < data.Count; i++)
-    //    {
-    //        // 3.1 Get PileObject Data
 
-    //        // Get pile object attribute names and values in a dictionary format
-    //        //Dictionary<string, string> pileObjData = data[i];
+    public void write(string[] data, string worksheetName, string[] cellAddresses)
+    {
+        // 1. OPEN/CREATE EXCEL WORKSHEET
+        this.activateWorksheet(worksheetName);
 
-    //        // 3.2 Create Headers
+        for (int i = 0; i < data.Length; i++)
+        {
+            this.ExcelWks.Range[cellAddresses[i]].Value = data[i];   
+        }
+    }
 
-    //        if (i == 0)
-    //        {
-    //            // Create Secondary Excel Headers with the names of the pile object attributes
-    //            int k = 0;
-    //            pileObjData.Keys.ToList().ForEach(key =>
-    //            {
-    //                ExcelApp.ActiveCell.Offset[0, k].Value = key;
-    //                k++;
-    //            });
+    public void write<T>(List<T> data, string worksheetName, string startCellAddress, string[] headerTitles = null) where T: SteelFrame
+    {
+        // 1. OPEN/CREATE EXCEL WORKSHEET
+        this.activateWorksheet(worksheetName);
 
-    //            // Create Primary Excel Header with input header title if provided
-    //            if (headerTitle != "")
-    //            {
-    //                Range headerRange = ExcelApp.Range[ExcelApp.ActiveCell.Offset[-1, 0],
-    //                    ExcelApp.ActiveCell.End[XlDirection.xlToRight].Offset[-1, 0]];
-    //                headerRange.Merge();
-    //                headerRange.Value = headerTitle;
-    //                FormatRange(headerRange, ExcelRangeType.HEADER_PRIMARY);
-    //            }
+        // 2. POSITION ACTIVE CELL
+        if (ExcelWks.Range[startCellAddress] != null) ExcelWks.Range[startCellAddress].Activate();
+        else ExcelWks.Range["A1"].Activate();
 
-    //            // Assign Range Format to Secondary Excel Headers
-    //            Range subHeaderRange = ExcelApp.Range[ExcelApp.ActiveCell,
-    //                ExcelApp.ActiveCell.End[XlDirection.xlToRight]];
-    //            FormatRange(subHeaderRange, ExcelRangeType.HEADER_SECONDARY);
-    //        }
+        // 3. WRITE DATA IN THE WORKSHEET
+        for (int i = 0; i < data.Count; i++)
+        {
+            if ((headerTitles!=null) && (i==0))
+            {
+                for (int j = 0; j < headerTitles.Length; j++)
+                {
+                    ExcelApp.ActiveCell.Offset[0,j].Value=headerTitles[j];
+                }
+                ExcelApp.ActiveCell.Offset[1, 0].Activate();
+                break;
+            }
 
-    //        // 3.3 Write PileObject Data into the cells below the headers
-    //        for (int j = 0; j < pileObjData.Count; j++)
-    //        {
-    //            ExcelApp.ActiveCell.Offset[i + 1, j].Value = pileObjData.Values.ElementAt(j);
-    //        }
-    //    }
-    //}
+            ExistingSteelFrame existingFrame = data[i] as ExistingSteelFrame;
+
+            this.ExcelApp.ActiveCell.Value= existingFrame.getSectionType().ToString();
+            this.ExcelApp.ActiveCell.Offset[0,1].Value= existingFrame.getSection().getName();
+            this.ExcelApp.ActiveCell.Offset[0,2].Value = existingFrame.getSection().getArea();
+            this.ExcelApp.ActiveCell.Offset[0,3].Value = existingFrame.getLength();
+            this.ExcelApp.ActiveCell.Offset[0,4].Value = existingFrame.getReuseRating().ToString();
+
+            this.ExcelApp.ActiveCell.Offset[1,0].Activate();
+        }
+    }
+
 
     public void SetFilePath(string filePath)
     {
@@ -158,7 +158,7 @@ public class ExcelDataManager
         this.filePath = filePath;
     }
 
-    public void Dispose()
+    public void dispose()
     {
         if (this.filePath != "")
         {
@@ -168,27 +168,27 @@ public class ExcelDataManager
         }
     }
 
-    //public void FormatRange(Range range, ExcelRangeType excelRangeType)
-    //{
-    //    // Assign specific Range Format based on input ExcelRangeType Enum value
-    //    switch (excelRangeType)
-    //    {
-    //        case ExcelRangeType.HEADER_PRIMARY:
-    //            // Call private FormatRange() method with pre-set inputs
-    //            FormatRange(range, "Segoe UI", 10, true, XlHAlign.xlHAlignCenter, XlVAlign.xlVAlignCenter, XlLineStyle.xlContinuous,
-    //                XlBorderWeight.xlMedium, XlColorIndex.xlColorIndexNone, XlPattern.xlPatternLinearGradient, 90,
-    //                XlThemeColor.xlThemeColorAccent6, false);
-    //            break;
-    //        case ExcelRangeType.HEADER_SECONDARY:
-    //            // Call private FormatRange() method with pre-set inputs
-    //            FormatRange(range, interiorColor: 19);
-    //            break;
-    //        case ExcelRangeType.NORMAL:
-    //            // Call private FormatRange() method with pre-set inputs
-    //            FormatRange(range);
-    //            break;
-    //    }
-    //}
+    public void FormatRange(Range range, ExcelRangeType excelRangeType)
+    {
+        // Assign specific Range Format based on input ExcelRangeType Enum value
+        switch (excelRangeType)
+        {
+            case ExcelRangeType.HEADER_PRIMARY:
+                // Call private FormatRange() method with pre-set inputs
+                FormatRange(range, "Segoe UI", 10, true, XlHAlign.xlHAlignCenter, XlVAlign.xlVAlignCenter, XlLineStyle.xlContinuous,
+                    XlBorderWeight.xlMedium, XlColorIndex.xlColorIndexNone, XlPattern.xlPatternLinearGradient, 90,
+                    XlThemeColor.xlThemeColorAccent6, false);
+                break;
+            case ExcelRangeType.HEADER_SECONDARY:
+                // Call private FormatRange() method with pre-set inputs
+                FormatRange(range, interiorColor: (XlColorIndex)19);
+                break;
+            case ExcelRangeType.NORMAL:
+                // Call private FormatRange() method with pre-set inputs
+                FormatRange(range);
+                break;
+        }
+    }
 
     private void FormatRange(Range range, string fontName = "Segoe UI", int fontSize = 10, bool fontBold = false,
     XlHAlign textHorAlignment = XlHAlign.xlHAlignCenter, XlVAlign textVertAlignment = XlVAlign.xlVAlignCenter,
@@ -227,5 +227,10 @@ public class ExcelDataManager
 
         // Autofit
         if (autoFit) range.EntireColumn.AutoFit();
+    }
+
+    public void refreshWorkbook()
+    {
+        this.ExcelWkb.RefreshAll();
     }
 }
