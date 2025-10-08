92fa266c3207572f59c9e8a9c4b1758f6e3ac32d
GCRA101 <126593217+GCRA101@users.noreply.github.com>
2025-10-08 17:51:03 +0100

    Switch from BHE_Filter Comments 01 to BHE_Reuse Rating

--- ReuseSchemeTool/model/AppConfig.cs
+++ ReuseSchemeTool/model/AppConfig.cs
@@ -13,7 +13,7 @@ namespace ReuseSchemeTool.model
         public const string PARAM_REUSE_STRATEGY = "BHE_Reuse Strategy";
         public const string PARAM_STRUCTURAL_MATERIAL = "BHE_Material";
         public const string PARAM_PHASE = "Existing";
-        public const string PARAM_REUSE_RATING = "BHE_Filter Comments 01";
+        public const string PARAM_REUSE_RATING = "BHE_Reuse Rating";
         public const string TITLEBLOCK_FAMILY_NAME = "BHE_TitleBlocks_A0-A1-A2_RST";
         public const string TITLEBLOCK_TYPE_NAME = "BHE_A1";
     }
